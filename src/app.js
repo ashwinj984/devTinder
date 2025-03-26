0b3cd31be5504fe8b3f85e0bb07b25dc6140a93e
Ashwin Jain <ashwinj984@gmail.com>
2025-03-26 23:13:46 +0530

    playing with get post and delete request

--- src/app.js
+++ src/app.js
@@ -2,17 +2,22 @@ const express = require('express');
 
 const app  = express();
 
+
+// this will only handle GET requests to /user
+app.get("/user", (req, res) => {
+     res.send({firstName : "Ashwin", lastName : "Jain"});
+})
+
+app.post("/user", (req, res) => {
+    console.log("Save data to the database")
+    res.send("User data saved successfully");
+})
+
+
+
+// this will match all the HTTP method API calls to /test
 app.use("/test",(req, res) => {
     res.send("Hello from the test side");
-});
-
-
-app.use("/hello",(req, res) => {
-    res.send("Hello from the Hello side");
-});
-
-app.use("/",(req, res) => {
-    res.send("Hello from the server side");
 });
 
 
