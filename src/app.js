12b94ef8f9099957cb3a6d641da2708fb10cb3e6
Ashwin Jain <ashwinj984@gmail.com>
2025-03-28 20:14:20 +0530

    Encrypted passwords and compared then and made a small login api

--- src/app.js
+++ src/app.js
@@ -37,6 +37,25 @@
         }
     });
 
+    app.post("/login", async(req, res)=> {
+        try{
+            const { emailId, password} = req.body;
+            const user = await User.findOne({emailId: emailId});
+            if(!user){
+                throw new Error("Invalid credentials");
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, user.password)
+            if(isPasswordValid){
+                res.send("Login Successful!!!");
+            }else{
+                throw new Error("Invalid credentials");
+            }
+        }catch(err){
+            res.status(400).send("Error: " + err.message)
+        }
+    })
+
     // GET user by email
     app.get("/user", async (req, res) => {
         // console.log(req.body.emailId);
