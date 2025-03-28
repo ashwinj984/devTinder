cbcf8d2ccaf3c8864a5c3313afc47c8ef142f84a
Ashwin Jain <ashwinj984@gmail.com>
2025-03-28 19:12:40 +0530

    Validate data in signup api + added brycpt package for hashed password

--- src/app.js
+++ src/app.js
@@ -4,18 +4,36 @@
 
     const app  = express();
     const User = require("./models/user");
-
+    const { validateSignUpData } = require("./utils/validation");
+    const bcrypt = require('bcrypt');
 
     app.use(express.json())
 
     app.post("/signup", async (req, res) => {
 
-        const user = new User(req.body);
+        // Create a new instance of the User model
         try{
+            // Validation of the data
+            validateSignUpData(req);
+            
+            const  {firstName, lastName, emailId,  password} = req.body;
+            // Encrypt the password
+
+            const passwordHash = await bcrypt.hash(password, 10);
+            const user = new User(
+                {
+                    firstName,
+                    lastName,
+                    emailId, 
+                    password : passwordHash,
+                }
+            );
+
+
             await user.save();
             res.send("User Added Successfully");
         }catch(err){
-            res.status(400).send("Error saving the user")
+            res.status(400).send("Error saving the user " + err.message)
         }
     });
 
