8b835fa44cd01d2e783b34ea283373ba33c267ed
Ashwin Jain <ashwinj984@gmail.com>
2025-03-29 13:56:15 +0530

    Formatted code and played with JWT tokens and cookies

--- src/app.js
+++ src/app.js
@@ -6,8 +6,14 @@ const app = express();
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
-app.use(express.json())
+// parse data to json then only we will be able to read it
+app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
 
@@ -47,6 +53,11 @@ app.post("/login", async (req, res) => {
 
         const isPasswordValid = await bcrypt.compare(password, user.password)
         if (isPasswordValid) {
+
+            // Create a JWT token
+            const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$8546");
+            // Add the token to cookie and send the response back to the user
+            res.cookie("token", token);
             res.send("Login Successful!!!");
         } else {
             throw new Error("Invalid credentials");
@@ -56,9 +67,30 @@ app.post("/login", async (req, res) => {
     }
 })
 
+app.get("/profile", async (req, res) => {
+    try {
+        const cookie = req.cookies;
+        const { token } = cookie;
+        if (!token) {
+            throw new Error("Invalid Token");
+        }
+
+        //validate my token
+        const decodedValue = await jwt.verify(token, "DEV@Tinder$8546");
+
+        const { _id } = decodedValue;
+        const user = await User.findById(_id);
+        if (!user) {
+            throw new Error("User does not exist")
+        }
+        res.send("Data fetched successfully");
+    } catch (err) {
+        res.status(400).send("Error : " + err.message)
+    }
+})
+
 // GET user by email
 app.get("/user", async (req, res) => {
-    // console.log(req.body.emailId);
     const userEmail = req.body.emailId;
 
     try {
